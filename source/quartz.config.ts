4ad674b9fce8e44e19b3dd72cea7b5c6a92c819f
triakshal <triakshal@gmail.com>
2025-05-14 22:35:27 -0400

    vault backup: 2025-05-14 22:35:27

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "TL NOTES",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
