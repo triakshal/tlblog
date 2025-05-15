72b6b7bc1ba730062b747b8c5fe3588c8b9b49a0
triakshal <triakshal@gmail.com>
2025-05-15 11:19:40 -0400

    vault backup: 2025-05-15 11:19:40

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -41,7 +41,7 @@ export const defaultContentPageLayout: PageLayout = {
     repelForce: 0.5, // how much nodes should repel each other
     centerForce: 0.3, // how much force to use when trying to center the nodes
     linkDistance: 30, // how long should the links be by default?
-    fontSize: 0.6, // what size should the node labels be?
+    fontSize: 0.2, // what size should the node labels be?
     opacityScale: 1, // how quickly do we fade out the labels when zooming out?
     removeTags: [], // what tags to remove from the graph
     showTags: true, // whether to show tags in the graph
@@ -54,7 +54,7 @@ export const defaultContentPageLayout: PageLayout = {
     repelForce: 0.5,
     centerForce: 0.3,
     linkDistance: 30,
-    fontSize: 0.6,
+    fontSize: 0.2,
     opacityScale: 1,
     removeTags: [], // what tags to remove from the graph
     showTags: true, // whether to show tags in the graph
