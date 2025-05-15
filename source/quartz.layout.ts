efa0ff4556260ba2e1aa618c5bfd946dd758e1e6
triakshal <triakshal@gmail.com>
2025-05-15 11:06:55 -0400

    vault backup: 2025-05-15 11:06:54

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -27,8 +27,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    //Component.DesktopOnly(Component.Explorer()),
-    Component.Explorer(),
+    Component.DesktopOnly(Component.Explorer()),
+    //Component.Explorer(),
   ],
   right: [
     Component.Graph(),
@@ -45,8 +45,8 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    //Component.DesktopOnly(Component.Explorer()),
-    Component.Explorer(),
+    Component.DesktopOnly(Component.Explorer()),
+    //  Component.Explorer(),
   ],
   right: [],
 }
